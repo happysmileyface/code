94969139ef9ebfc72656debb1abbc0977e92bbb5
happysmileyface <95998572+happysmileyface@users.noreply.github.com>
2021-12-12 00:02:15 -0800

    Delete python directory

--- python/tkintertemplate.py
+++
@@ -1,8 +0,0 @@
-import tkinter as tk
-window = tk.Tk()
-Width = 1000
-Height = 600
-window.geometry(str(Width)+"x"+str(Height))
-window.title("Tkinter App Title")
-if __name__ == "__main__":
-    window.mainloop()
